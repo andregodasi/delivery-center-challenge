e8c7eb5dc1eb906c547726142ff58c1c8a402c46
andregodasi <andre.godasi@gmail.com>
2021-07-23 01:36:45 -0300

    chore: create project and config: * Type Script * Ant Design * Editor Config * Eslint * Prettier

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Button } from 'antd';
+import './App.css';
+
+const App: React.FC = () => {
+  return (
+    <div className='App'>
+      <Button type='primary' shape='round'>
+        TESTE
+      </Button>
+    </div>
+  );
+};
+
+export default App;
